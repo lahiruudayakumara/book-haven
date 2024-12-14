9175ac47ba3cd4881941f7cdd824dce147230e45
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-12-15 00:00:00 +0530

    Add API request functions for fetching all books and subscribing users

---
+++ lib/api-requests/book/all-books.ts
@@ -0,0 +1,39 @@
+import { IBook, IGetBooK, IPostBook } from "@/types/book";
+
+import ErrorResponse from "@/types/error-response";
+
+export default async function allBooks(book: IPostBook): Promise<
+    IGetBooK | ErrorResponse
+> {
+
+    try {
+        const data = new FormData();
+
+        if (book.page) {
+            data.append("page", book.page.toString())
+        }
+
+        if (book.limit) {
+            data.append("limit", book.limit.toString())
+        }
+
+        if (book.category) {
+            data.append("category", book.category)
+        }
+
+
+        const response = await fetch("/api/book", {
+            method: "POST",
+            body: data
+        });
+        if (!response.ok) {
+            throw new Error("Failed to fetch books");
+        }
+        const books = await response.json();
+        console.log(books)
+        return books;
+    } catch (error) {
+        console.error("Error:", error);
+        return { error: "Internal server error" };
+    }
+}
---
+++ lib/api-requests/subscribe/post-subscribe.ts
@@ -0,0 +1,33 @@
+import ErrorResponse from "@/types/error-response";
+import Logger from "@/utils/logger";
+
+export default async function postSubscribe(email: string): Promise<ErrorResponse | null> {
+    try {
+        if (!email) {
+            return { error: "Email is required" };
+        }
+
+        const formData = new FormData();
+
+        formData.append("email", email);
+
+        const response = await fetch("/api/subscribe", {
+            method: "POST",
+            body: formData,
+        });
+
+        if (!response.ok) {
+            Logger.error(
+                "Failed to subbscribe . Status:",
+                response.status
+            );
+            return { error: "Failed to subscribe" };
+        }
+
+        return await response.json();
+    } catch (error: any) {
+        Logger.error("Error:", error);
+        return { error: "Failed to subscribe" };
+    }
+
+}
