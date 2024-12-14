1024f0c3723d247f1c4b584b2ef86aa542423dcf
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-12-15 00:00:00 +0530

    Add Layout component with Stripe Elements and header/footer integration

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { Elements } from "@stripe/react-stripe-js";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Header /><Elements stripe={stripePromise}>
+      <div className="min-h-full">
+      {children}
+      </div>
+      </Elements>
+      <Footer />
+    </div>
+  );
+}
