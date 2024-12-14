a5c5f63a273eecc4433e941e411d6b77ed8fff53
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-12-15 00:00:00 +0530

    Add TypeScript interfaces for book data and a logger utility class

---
+++ types/book.ts
@@ -0,0 +1,24 @@
+export interface IBook {
+  id: string;
+  Title: string;
+  Price: GLfloat;
+  DiscountPrice: GLfloat;
+  Category: string;
+  Writer: string;
+  BookURL: string;
+  ImageURL: string;
+  Description: string;
+}
+
+export interface IPostBook {
+  category?: string,
+  page?: number,
+  limit?: number
+}
+
+export interface IGetBooK {
+  books: IBook[],
+  currentPage: number,
+  totalCount: number,
+  totalPages: number
+}
