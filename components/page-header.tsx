4a3fa10b43220cd3c4744aa23d34d5206d06f83c
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-12-18 23:23:24 +0530

    ui improve: adjust layout for cart page and page header for better responsiveness

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const PageHeader: React.FC<Props> = ({ title, description }) => {
   return (
     <div className="flex flex-col h-auto items-center w-full py-14 bg-slate-100 dark:bg-gray-800">
-      <div className="w-full px-4 2xl:px-0 lg:w-2/3 h-auto">
+      <div className="w-full px-4 2xl:px-0 2xl:w-2/3 h-auto">
         <h1 className="text-xl font-bold mb-2 dark:text-white">{title}</h1>
         <p className="text-gray-600 dark:text-gray-200">{description}</p>
       </div>
